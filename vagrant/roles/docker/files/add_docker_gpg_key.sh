74d440ab8862633bae58942ea15ba397d29c380c
Mario Schwabe Filho <marioschwabefilho@gmail.com>
2023-07-19 16:01:49 +0200

    Added new roles java, python and tools. Updated roles chrome, docker, kubernetes, ubuntu-desktop and user

--- vagrant/roles/docker/files/add_docker_gpg_key.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
--- vagrant/roles/kubernetes/files/download_google_cloud_sign_key.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
